e565bbc229e56e450243202bb03a61d56e21a05f
Mariusz Fik <mariusz@fidano.pl>
2015-08-20 02:15:21 +0200

    Implement game :D

--- app/src/main/java/pl/fidano/magicnumber/MainActivity.java
+++ app/src/main/java/pl/fidano/magicnumber/MainActivity.java
@@ -1,16 +1,40 @@
 package pl.fidano.magicnumber;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String REMOTE_URL = "http://sluchaj.radiooswiecim.pl:8000/status-json.xsl";
+    private TextView timestamp, lucky;
+    private Button roll;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initControls();
+        updateTimestamp();
+        new StatusHandler().execute(REMOTE_URL);
     }
 
     @Override
@@ -34,4 +58,60 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void initControls() {
+        timestamp = (TextView) findViewById(R.id.timestamp);
+        lucky = (TextView) findViewById(R.id.lucky);
+        roll = (Button) findViewById(R.id.roll);
+        roll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.roll:
+                updateTimestamp();
+                new StatusHandler().execute(REMOTE_URL);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void updateTimestamp() {
+        timestamp.setText(sdf.format(new Date()));
+    }
+
+    public class StatusHandler extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... params) {
+
+            int listeners = 0;
+
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                String json = IOUtils.toString(in, "UTF-8");
+
+                JSONObject stats = new JSONObject(json);
+                JSONObject server = stats.getJSONObject("icestats");
+                listeners = server.getJSONObject("source").getInt("listeners");
+
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error while geting data.", Toast.LENGTH_SHORT).show();
+            }
+            return listeners;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            TextView lucky = (TextView) findViewById(R.id.lucky);
+            lucky.setText(result.toString());
+        }
+    }
+
 }
